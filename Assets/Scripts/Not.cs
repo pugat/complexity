c45ca7d2fc17e0db652b320fd9c7f2e47e3947aa
Mahmut Can <mahmutcanucanefe@gmail.com>
2017-03-09 19:34:24 +0300

    Update-Versıon1

---
+++ Assets/Scripts/Not.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Not : MonoBehaviour {
+
+public GameObject _textTransform;
+//public Text _text;
+bool not = false;
+
+void Update(){
+	if(not == true && Input.GetKeyDown(KeyCode.E)){
+		_textTransform.SetActive(true);
+	
+	}
+}
+
+	void OnTriggerEnter(Collider coll){
+		if(coll.tag == "Player")
+		{
+			Debug.Log("E ye bas");
+			not = true;
+			StartCoroutine(NotI());
+		}
+	}
+	
+	IEnumerator NotI(){
+		yield return new WaitForSeconds(5);
+		_textTransform.SetActive(false);
+		not = false;
+	}
+}
