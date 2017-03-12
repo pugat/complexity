1b67e787d263d44b473655d6d109d90e67105ff0
Mahmut Can <mahmutcanucanefe@gmail.com>
2017-03-12 16:20:41 +0300

    Big-Update
    Last

---
+++ Assets/Scripts/Demre/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+ public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Menu()
+    {
+	
+    }
+}
